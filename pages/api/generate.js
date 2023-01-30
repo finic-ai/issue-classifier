fabc30d67fdfce20c1c4e8af02ae1d3f505353cb
Jason Fan <jason.wc.fan@gmail.com>
2023-01-30 09:42:29 -0800

    updated prompts

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,11 +1,66 @@
 import { Configuration, OpenAIApi } from "openai";
 
+const getPrompt = (promptName, inquiry) => {
+  switch (promptName) {
+    case 'issue_classification':
+      return `You are an employee at Home Depot tasked with classifying the type of issue a customer is reporting. If the issue does not apepar to be related to Home Depot or its products, say 'Unrelated' instead of making something up.
+      
+      You must choose from the following options:
+      - Order Status
+      - Shipping and Delivery
+      - Products and Services
+      - Pricing and Promos
+      - Payments
+      - Account
+      - Returns
+      - Policies and Legal
+      - Corporate Information
+      - Speak to a Human
+  
+      Question: Can I return orders online?
+      Answer: Returns
+      
+      Question: When will my order arrive?
+      Answer: Shipping and Delivery
+      
+      Question: Let me speak to someone
+      Answer: Speak to a Human
+
+      Question: I can't sign in to my account
+      Answer: Account
+
+      Question: I need to get a refund, my drill bit arrive broken
+      Answer: Products and Services
+
+      Question: Do you stock the ryobi cordless sander?
+      Answer: Products and Services
+      
+      Question: ${inquiry}
+      Answer:`
+    case 'question_and_answer':
+      return `You are a customer support agent for Home Depot.
+  
+      Question: Can I return orders online?
+      Answer: Home Depot offers free and easy returns online, wherever you want.
+      
+      Question: When will my order arrive?
+      Answer: Estimated delivery dates are provided for each item in the product information page. To check the status of your order online, please visit The Home Depot order status page. Enter your order confirmation number and email address in the space provided. You can also track your item by clicking on the tracking number in the Shipping Confirmation e-mail that is sent once your item ships.
+      
+      Question: Are there any delivery restrictions?
+      Answer: Any states with delivery restrictions will be noted on each item’s product information page. For items eligible for delivery to Alaska and Hawaii, please allow an additional 2-5 business days. Additional remote surcharges may apply. We are not able to ship to international destinations, PO Boxes, APO/FPO/DPO addresses at this time.
+      
+      Question: ${inquiry}
+      Answer:`
+  }
+}
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-export default async function (req, res) {
+export default async function generateResponse(req, res) {
+  console.log('here')
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
@@ -15,11 +70,11 @@ export default async function (req, res) {
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const inquiry = req.body.inquiry || '';
+  if (inquiry.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Please enter a valid inquiry",
       }
     });
     return;
@@ -28,9 +83,10 @@ export default async function (req, res) {
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
+      prompt: generatePrompt(inquiry),
       temperature: 0.6,
     });
+    console.log(completion.data)
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
@@ -48,15 +104,6 @@ export default async function (req, res) {
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+function generatePrompt(inquiry) {
+  return getPrompt('issue_classification', inquiry);
 }
